21b5cff3b9823c5af76afb893a4e9433dc01ec4a
jaebungs <jaebungs@gmail.com>
2020-10-14 18:57:14 -0400

    New dimension function finished

--- ETCH-A-SKETCH/index.js
+++ ETCH-A-SKETCH/index.js
@@ -2,7 +2,7 @@ const containerEl = document.querySelector('.container')
 const btnContainerEl = document.querySelector('.btn-container')
 let dimension = 16
 
-//Need to create grid 16 * 16, total 196 divs.
+//Need to create grid ex) 16 * 16, total 196 divs.
 const makeGrid = () => {
     let totalgrids = Math.pow(dimension, 2);
     containerEl.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
@@ -18,7 +18,11 @@ const makeGrid = () => {
     }
 
     makeButtons()
+    resetGrids()
+    getNewDimension()
 }
+
+
 
 //Make buttons for reset, color and new dimension
 const makeButtons = () => {
@@ -51,6 +55,55 @@ const makeButtons = () => {
     colorButton.style.height = '40px'
     btnContainerEl.appendChild(colorButton)
 }
+
+// const clearGrids = () => {
+    
+// }
+
+// Reset all grids.
+const resetGrids = () => {
+    const resetEl = document.querySelector('.reset-btn')
+    const allGrids = document.querySelectorAll('.grid')
+    
+    //When reset btn is clicked, change all grids backgorund color to white
+    resetEl.addEventListener('click', function(){
+        for (let i=0; i < allGrids.length; i++) {
+            allGrids[i].style.background = '#fff'
+        }
+    })
+}
+
+// Get new dimension.
+const getNewDimension = () => {
+    let newDimension = document.querySelector('.dimension-btn')
+    
+    newDimension.addEventListener('click', function(){
+        let userInput = prompt('Please put number between 10 - 100: ')
+        
+        if (userInput > 10 || userInput < 100) {
+            dimension = userInput
+            
+            //Need to reset the page to render? new grids
+            containerEl.innerHTML = ``
+            btnContainerEl.innerHTML = ``
+            return makeGrid()
+        } else {
+            return prompt('Error! Inser between 10 - 100')
+        }
+        
+    })
+}
+
+
+
+// Change grid background when mouseover.
+document.addEventListener('mouseover', function(e){
+    // Get id of a grid
+    let girdId = e.target.getAttribute('id')
+    if (e.target.getAttribute('id')) {
+        document.getElementById(girdId).style.backgroundColor = '#000'
+    }
+})
 
 
 makeGrid()
