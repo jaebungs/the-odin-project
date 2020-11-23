ea083dcd723569be02ae857d5ec43f753cbf8e38
jaebungs <jaebungs@gmail.com>
2020-11-23 15:18:30 -0500

    update summary.js edit function

--- CV-app-react/src/components/Summary.js
+++ CV-app-react/src/components/Summary.js
@@ -4,24 +4,52 @@ export default class Summary extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            summary: ``,
+            summary: `Default Summary`,
             isEditing: false
         };
         this.handleSummaryEdit = this.handleSummaryEdit.bind(this);
-
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     handleSummaryEdit(){
+        this.setState({
+            isEditing: true
+        })
+    }
 
+    handleChange(e){
+        this.setState({
+            summary: e.target.value.trim()
+        })
+    }
+
+    handleSubmit(){
+        this.setState({
+            isEditing: false
+        })
     }
 
     render(){
-        
-        const { summary, isEditing } = this.state
-        return(
+        const { summary, isEditing } = this.state;
+
+        const viewTemplate = (
             <div>
-            Summary
+                <button className="add-btn" onClick={this.handleSummaryEdit}>Edit</button>
+                <p className="summary-text">{summary}</p>
             </div>
+        )
+
+        const editTemplate = (
+            <div>
+                <label htmlFor="summary">Summary Edit:</label>
+                <textarea id="summary" name="summary" onChange={this.handleChange} value={summary}></textarea>
+                <button onClick={this.handleSubmit}>Update</button>
+            </div>
+        )
+
+        return(
+            <div>{isEditing ? editTemplate : viewTemplate}</div>
         )
     }
     
